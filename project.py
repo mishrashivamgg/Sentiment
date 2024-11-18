0f5385ebb0b824253a568011856207250b80a3eb
mishrashivamgg <94074838+mishrashivamgg@users.noreply.github.com>
2024-11-18 19:45:21 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,38 @@
+import streamlit as st
+from textblob import TextBlob
+st.sidebar.title("about us")
+st.sidebar.text("we are student at Ducat ")
+
+
+st.sidebar.title("Contact us")
+st.sidebar.text("""monu @1111
+sonu @2222
+monu @3333""")
+
+st.title("Sentiment Analysis")
+
+text=st.text_input("**enter text**")
+btn=st.button("predict")
+
+if btn:
+    blob=TextBlob(text)
+    sent=blob.sentiment[0]
+    if sent<0:
+        st.error("Negative Sentiment")
+        st.image("C:/duct_pandas/Neg.jpg")
+    elif sent>0:
+        st.success("Possitive Sentiment")
+        st.image("C:/duct_pandas/pos.jpg")
+    else:
+        st.warning("Neutral Sentiment")
+        st.image("C:/duct_pandas/neu.jpg")
+
+
+
+
+
+
+
+
+
+    
